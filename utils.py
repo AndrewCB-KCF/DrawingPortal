3fe1536c7a469105fe3eed4404960bf741eaff9a
AndrewCB-KCF <andrew.breier@kcftech.com>
2026-08-24 15:15:30 -0400

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,62 @@
+import sqlite3
+import pandas as pd
+
+DB_PATH = "C:\\Users\\andrew.breier\\Documents\\DatabaseBackups\\SMARTDrawings Database-KCF-4DPWP74 - Copy.db"
+
+
+def get_connection():
+    return sqlite3.connect(DB_PATH)
+
+
+def get_drawings():
+
+    conn = get_connection()
+
+    df = pd.read_sql(
+        """
+        SELECT *
+        FROM drawings
+        ORDER BY drawing_number
+        """,
+        conn
+    )
+
+    conn.close()
+
+    return df
+
+
+def get_revision_history():
+
+    conn = get_connection()
+
+    df = pd.read_sql(
+        """
+        SELECT *
+        FROM revision_history
+        ORDER BY created_date DESC
+        """,
+        conn
+    )
+
+    conn.close()
+
+    return df
+
+
+def get_approvals():
+
+    conn = get_connection()
+
+    df = pd.read_sql(
+        """
+        SELECT *
+        FROM approvals
+        ORDER BY approval_date DESC
+        """,
+        conn
+    )
+
+    conn.close()
+
+    return df
