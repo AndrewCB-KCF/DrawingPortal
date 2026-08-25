838c52197a5db0d057e3e2add106393a57a3c187
AndrewCB-KCF <andrew.breier@kcftech.com>
2026-08-25 09:43:34 -0400

    Update 1_Drawings.py

--- pages/1_Drawings.py
+++ pages/1_Drawings.py
@@ -160,6 +160,7 @@ selected_rows = grid_response.get("selected_rows")
 
 if selected_rows is not None and len(selected_rows) > 0:
 
+    st.write(selected_rows)
     drawing_number = selected_rows[0]["drawing_number"]
 # if selected_rows is not None and len(selected_rows) > 0:
 
